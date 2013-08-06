917c574858a908245ffdc8d11b6207dd837bfd6c
Ben Barreth <websiteguru@gmail.com>
2013-08-05 23:29:52 -0500

    first commit

---
+++ MyBlobContainer.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AzureMassUploader
+{
+
+	internal class MyBlobContainer
+	{
+		public MyBlobContainer() { }
+
+		internal CloudBlobContainer GetBlobContainerByName(string containerName)
+		{
+			containerName = MakeSafeAzureContainerName(containerName);
+
+			//Create service client for credentialed access to the Blob service.
+			CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["AzureStorageConnectionString"].ConnectionString);
+			CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+			//Get a reference to a container, which may or may not exist.
+			CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+			return container;
+		}
+
+		internal CloudBlobContainer CreateBlobContainerIfNotExists(string containerName)
+		{
+			CloudBlobContainer container = GetBlobContainerByName(containerName);
+			container.CreateIfNotExists();
+			container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+			Console.WriteLine("Container created with name: " + container.Name);
+
+			return container;
+		}
+
+		public static string MakeSafeFileName(string unsafeName)
+		{
+			unsafeName = HttpUtility.HtmlDecode(unsafeName);
+
+			// swap spaces with underscore
+			unsafeName = Regex.Replace(unsafeName, @"[\s]", "-");
+
+			// strip all non-alpha chars 
+			unsafeName = Regex.Replace(unsafeName, @"[^A-Za-z0-9-]", "");
+
+			return unsafeName;
+		}
+
+		public static string MakeSafeAzureContainerName(string unsafeName)
+		{
+			return MakeSafeFileName(unsafeName).TrimStart('-').ToLower();
+		}
+
+		public string GetImageContentType(string fileExtension)
+		{
+			string contentType = "";
+
+			switch (fileExtension.ToLower())
+			{
+				case ".png":
+					contentType = "image/png";
+					break;
+				case ".gif":
+					contentType = "image/gif";
+					break;
+				case ".jpg":
+					contentType = "image/jpg";
+					break;
+				case ".jpeg":
+					contentType = "image/jpeg";
+					break;
+				case ".bmp":
+					contentType = "image/bmp";
+					break;
+				case ".tif":
+				case ".tiff":
+					contentType = "image/tiff";
+					break;
+				default:
+					Console.WriteLine("This file extension is not a jpg, jpeg, png or gif: " + contentType);
+					break;
+			}
+			Console.WriteLine("ContentType is set to: " + contentType);
+			return contentType;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace AzureMassUploader
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string locationOfFolderToUpload = ConfigurationManager.AppSettings.Get("ImageFolderLocation"); 
+			bool PauseAfterEachFile = bool.Parse(ConfigurationManager.AppSettings.Get("PauseAfterEachFile"));
+			string StartUploadAtCharacter = ConfigurationManager.AppSettings.Get("StartUploadAtFilesBeginningWithLetter");
+			bool UploadStarted = false; 
+
+			Console.WriteLine("Press any key to proceed.");
+			Console.ReadLine();
+
+			foreach (string fullLocalFilename in System.IO.Directory.GetFiles(locationOfFolderToUpload, "*.*", System.IO.SearchOption.AllDirectories))
+			{
+				try
+				{
+					string filenameWithSlashes = fullLocalFilename.Replace(locationOfFolderToUpload, "").Replace("\\", "/").TrimStart('/');
+					string containerName = filenameWithSlashes.Substring(0, filenameWithSlashes.IndexOf("/"));
+					if (containerName.Length < 3) containerName = containerName + "1";
+
+					filenameWithSlashes = filenameWithSlashes.Substring(filenameWithSlashes.IndexOf('/')).TrimStart('/').Replace("[","").Replace("]","");
+
+					if (!containerName.ToLower().StartsWith(StartUploadAtCharacter.ToLower()) && !UploadStarted) continue;
+
+					UploadStarted = true;
+
+					Console.WriteLine("Container name: " + containerName);
+					// if container doesn't exist, create it
+					MyBlobContainer myBlob = new MyBlobContainer();
+					CloudBlobContainer container = myBlob.CreateBlobContainerIfNotExists(containerName);
+
+					Console.WriteLine("Attempting to upload file: " + filenameWithSlashes);
+					//Get a reference to a blob, which may or may not exist.
+					CloudBlockBlob blob = container.GetBlockBlobReference(filenameWithSlashes);
+					blob.Properties.ContentType = myBlob.GetImageContentType(filenameWithSlashes.Substring(filenameWithSlashes.LastIndexOf('.')));
+					
+					using (MemoryStream ms = new MemoryStream(File.ReadAllBytes(fullLocalFilename)))
+					{
+						blob.UploadFromStream(ms);
+					}
+
+					Console.WriteLine("File Uploaded: " + filenameWithSlashes);
+					Console.WriteLine();
+
+					if (PauseAfterEachFile)
+					{
+						Console.WriteLine("Press any key to proceed.");
+						Console.ReadLine();
+					}
+				}
+				catch (Exception ex)
+				{
+					string errorMsg = "fullLocalFilename: " + fullLocalFilename + "\n ex.Message: " + ex.Message + "  \n ex.StackTrace: " + ex.StackTrace + "\n fullLocalFilename: " + fullLocalFilename;
+					System.IO.File.AppendAllText("ErrorLog.txt", errorMsg); 
+					Console.WriteLine(errorMsg);
+					Console.ReadLine();
+				}
+
+			}
+		}
+
+	}
+
+}
